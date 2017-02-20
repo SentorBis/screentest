be2cfaa4c0be40a35717bf891446b95c3a6cc8bc
Pegase - Grégoire <Pegase - Grgoire>
2017-02-20 16:41:15 -0500

    Trying to figure out how to set up the databas.

---
+++ base_concept.sql
@@ -0,0 +1,25 @@
+SET search_path = screentest;
+
+DROP SCHEMA IF EXISTS screentest CASCADE;
+CREATE SCHEMA screentest;
+
+
+DROP TABLE IF EXISTS screentest.Category;
+CREATE TABLE IF NOT EXISTS  screentest.Category (
+		cat_id			INTEGER		NOT NULL,
+		cat_name 		VARCHAR(20)	NOT NULL,
+		description VARCHAR(200)	NOT NULL,
+		PRIMARY KEY (cat_id));
+		
+
+DROP TABLE IF EXISTS screentest.Question;
+CREATE TABLE IF NOT EXISTS  screentest.Question (
+		screen_id		INTEGER		NOT NULL,
+		image			BYTEA	NOT NULL,
+		answer0 		VARCHAR(50)	NOT NULL,
+		answer1 		VARCHAR(50)	NOT NULL,
+		answer2 		VARCHAR(50)	NOT NULL,
+		answer3 		VARCHAR(50)	NOT NULL,
+		cat_id		INTEGER		NOT NULL,
+		PRIMARY KEY (screen_id),
+		FOREIGN KEY(cat_id) REFERENCES screentest.Category(cat_id) ON DELETE RESTRICT ON UPDATE CASCADE);
