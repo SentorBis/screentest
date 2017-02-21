7bae6c0537942a0773b4315494929b88f42e57a4
Pegase - Grégoire <Pegase - Grgoire>
2017-02-21 15:55:49 -0500

    Added an important column to the SQL schema.

--- base_concept.sql
+++ base_concept.sql
@@ -15,11 +15,12 @@ CREATE TABLE IF NOT EXISTS  screentest.Category (
 DROP TABLE IF EXISTS screentest.Question;
 CREATE TABLE IF NOT EXISTS  screentest.Question (
 		screen_id		INTEGER		NOT NULL,
-		image			BYTEA	NOT NULL,
+		image			BYTEA		NOT NULL,
 		answer0 		VARCHAR(50)	NOT NULL,
 		answer1 		VARCHAR(50)	NOT NULL,
 		answer2 		VARCHAR(50)	NOT NULL,
 		answer3 		VARCHAR(50)	NOT NULL,
-		cat_id		INTEGER		NOT NULL,
+		cat_id			INTEGER		NOT NULL,
+		validated		BOOLEAN		NOT NULL COMMENT, /*Une question ajoutée par un internaute n'est pas validée. Il faut validation de l'administrateur pour que la question soit utilisable. */
 		PRIMARY KEY (screen_id),
 		FOREIGN KEY(cat_id) REFERENCES screentest.Category(cat_id) ON DELETE RESTRICT ON UPDATE CASCADE);
