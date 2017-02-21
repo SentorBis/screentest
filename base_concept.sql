cd68072d0f099ba006fc0510d65866113d3c11a6
Pegase - Grégoire <Pegase - Grgoire>
2017-02-21 17:32:14 -0500

    Added INSERT commands to add basic rows to the Category table.

--- base_concept.sql
+++ base_concept.sql
@@ -24,3 +24,9 @@ CREATE TABLE IF NOT EXISTS  screentest.Question (
 		validated		BOOLEAN		NOT NULL COMMENT, /*Une question ajoutée par un internaute n'est pas validée. Il faut validation de l'administrateur pour que la question soit utilisable. */
 		PRIMARY KEY (screen_id),
 		FOREIGN KEY(cat_id) REFERENCES screentest.Category(cat_id) ON DELETE RESTRICT ON UPDATE CASCADE);
+
+INSERT INTO screentest.Category(cat_id, cat_name, description) VALUES (1,'Cinéma','La catégorie de tous les films, sans restriction de taille ou de forme.');
+INSERT INTO screentest.Category(cat_id, cat_name, description) VALUES (2,'Séries','La catégorie des séries télévisées (ou non) avec des vrais acteurs dedans.');
+INSERT INTO screentest.Category(cat_id, cat_name, description) VALUES (3,'Jeu vidéo','La catégorie des jeux vidéo de Pong à Grand Theft Auto V.');
+INSERT INTO screentest.Category(cat_id, cat_name, description) VALUES (4,'Dessins animés','La catégorie des séries animées, dessinées à la main ou animées en 3D.');
+
