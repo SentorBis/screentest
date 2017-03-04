8b5c72bf7c2bad0541144ff6ad6e46550c1d055d
Pegase - Grégoire <Pegase - Grgoire>
2017-03-04 17:34:12 -0500

    Work in progress: making the submit page work.

--- submit.php
+++ submit.php
@@ -21,11 +21,11 @@
 	</ul>
 	<center>
 	  <h1>Soumettre un screenshot :</h1>
-	  <form action="insert.php" method="post" enctype="multipart/form-data">
+	  <form action="submit.php" method="post" enctype="multipart/form-data">
 		<p><label>Screenshot (format JPG ou PNG, maximum 300Ko) :</label><br/>
 	    <input type="file" name="new_screenshot" ></p>
 		<p><label>Catégorie :</label><br/>
-		<select name="my_select">
+		<select name="category">
           <option value="1">Film</option>
           <option value="2">Série</option>
           <option value="3">Jeu vidéo</option>
@@ -38,8 +38,61 @@
         <input type="text" name="wanswer1" placeholder="Enter a wrong answer" maxlength="10">
         <input type="text" name="wanswer2" placeholder="Enter a wrong answer" maxlength="10">
         <input type="text" name="wanswer3" placeholder="Enter a wrong answer" maxlength="10"></p>
-        <p><button type="submit">Soumettre</button></p>
+        <p><button type="submit" name="submit">Soumettre</button></p>
       </form>
+	  
+	  <p><?php
+		ini_set('display_errors',1);
+		error_reporting(E_ALL | E_STRICT);
+	  if(isset($_POST['submit'])) {
+		  try {
+				$connec = new PDO('pgsql:host=localhost;port=5432;dbname=postgres;user=postgres;password=password');
+		  } catch (PDOException $e) {
+			  echo "Error : " . $e->getMessage() . "<br/><br/>";
+			  echo "The application failed to connect to the database.<br/>";
+			  die();
+		  }
+		  
+		
+		  if(isset($_POST['new_screenshot'])) {
+			$file = $_POST['new_screenshot'];
+			echo $file;
+		  } else {
+			  echo 'File not set';
+		  }
+		  
+		  /*$img = fopen($file_name, 'r') or die("Cannot read image\n");
+		  echo $file . ' : ' . filesize($file) . ' bytes.';*/
+		  /*$data = fread($img, filesize($file));
+		  $es_data = pg_escape_bytea($data);
+		  fclose($img);
+		  
+		  $cat = $_POST['category'];
+		  $a1 = $_POST['tanswer'];
+		  $a2 = $_POST['wanswer1'];
+		  $a3 = $_POST['wanswer2'];
+		  $a4 = $_POST['wanswer3'];*/
+		  
+/*$img = fopen($file_name, 'r') or die("cannot read image\n");
+$data = fread($img, filesize($file_name));
+
+$es_data = pg_escape_bytea($data);
+fclose($img);
+
+$query = "INSERT INTO images(id, data) Values(1, '$es_data')";
+pg_query($con, $query); 
+
+pg_close($con); 
+		  
+		  
+		  $sql = 'SELECT cat_id, cat_name, description FROM screentest.Category';
+		  foreach ($connec->query($sql) as $row) {
+			  print $row['cat_id'] . " ";
+			  print $row['cat_name'] . "--> ";
+			  print $row['description'] . "<br>";
+		  }*/
+	  }
+	  ?></p>
 	</center>
 	
 	<footer>
