2fe009e91e2284439608831ee0b1ec0f6a6e9523
Pegase - Grégoire <Pegase - Grgoire>
2017-03-13 14:46:05 -0400

    Implemented actual insertion of the data in database. Now fully functional.

--- submit.php
+++ submit.php
@@ -111,14 +111,14 @@
 			  echo 'Vérifiez que vous avez bien rempli tous les champs et choisi une catégorie valide.';
 		  } else {
 			  
-			  /****************CONNECTION BASE DE DONNEES [FONCTIONNE]
+			  /****************CONNECTION BASE DE DONNEES [FONCTIONNE]*/
 			  try {
 					$connec = new PDO('pgsql:host=localhost;port=5432;dbname=postgres;user=postgres;password=password');
 			  } catch (PDOException $e) {
 				  echo "Error : " . $e->getMessage() . "<br/><br/>";
 				  echo "The application failed to connect to the database.<br/>";
 				  die();
-			  }*/
+			  }
 			  
 			  //FILE UPLOAD
 			  //Le code de base provient de W3Schools, et a été arrangé pour les besoins du site.
@@ -138,23 +138,23 @@
 					//echo "File is an image - " . $check["mime"] . ".";
 					$uploadOk = 1;
 				} else {
-					echo "File is not an image.<br>";
+					echo "Votre fichier n'est pas une image.<br>";
 					$uploadOk = 0;
 				}
 			}
-			// Check if file already exists
-			if (file_exists($target_file)) {
-				echo "Sorry, file already exists.<br>";
-				$uploadOk = 0;
+			// Make triple extra sure the filename isn't taken
+			while (file_exists($target_file)) {
+				$target_file = $target_dir . generateUniqueFilename() . "." . end($tmp);
 			}
+			
 			// Check file size
 			if ($_FILES["new_screenshot"]["size"] > 300000) {
-				echo "Sorry, your file is too large.<br>";
+				echo "Votre fichier est trop lourd.<br>";
 				$uploadOk = 0;
 			}
 			// Allow certain file formats
 			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-				echo "Sorry, only JPG, JPEG & PNG files are allowed.<br>";
+				echo "Seuls les fichiers de type JPG, JPEG ou PNG sont acceptés.<br>";
 				$uploadOk = 0;
 			}
 			// Check if $uploadOk is set to 0 by an error
@@ -162,24 +162,38 @@
 				echo "Votre fichier n'a pas pu être uploadé.<br>";
 			// if everything is ok, try to upload file
 			} else {
+				
+				/*****************************************************************
+				**********UPLOAD APPROUVE, LE CODE SUIVANT L'UPLOAD EST ICI*******
+				*****************************************************************/
 				if (move_uploaded_file($_FILES["new_screenshot"]["tmp_name"], $target_file)) {
+					
 					echo "Le fichier ". basename( $_FILES["new_screenshot"]["name"]). " a été uploadé. <br>";
+					
+					$cat = $_POST['category'];
+					$a1 = $_POST['tanswer'];
+					$a2 = $_POST['wanswer1'];
+					$a3 = $_POST['wanswer2'];
+					$a4 = $_POST['wanswer3'];
+					
+					$sql = "INSERT INTO screentest.Question(image, answer0, answer1, answer2, answer3, cat_id) VALUES ('$target_file', '$a1', '$a2', '$a3', '$a4', '$cat')";
+					$connec->query($sql);
+					
+					// pour les tests on enlève le fichier après coup
+					//unlink($target_file);
+					//echo "Le fichier ". basename( $_FILES["new_screenshot"]["name"]). " a été subséquement viré parce que enlever des tonnes d'images de Maître Capello en boucle c'est pas mon idée du fun (virer la fonctionnalité après). <br>";
+					
 				} else {
 					echo "Désolé, il y a eu une erreur lors de l'upload de votre fichier, celui-ci n'a pas pu être mis en ligne.<br>";
 				}
 			}
+			// le PDO est automatiquement fermé
 		  }
 		  
 		  //RAPPEL : l'image a été uploadée et son adresse est $target_file
 		  
 		  //A FAIRE : mettre l'upload à proprement parler à la fin
 		  //A FAIRE : check nombre de caractères effectivement inputés (le dernier char ?)
-		  
-		  $cat = $_POST['category'];
-		  $a1 = $_POST['tanswer'];
-		  $a2 = $_POST['wanswer1'];
-		  $a3 = $_POST['wanswer2'];
-		  $a4 = $_POST['wanswer3'];
 		  
 /**************************TEMPLATE INSERTION IMAGE DB [?]
 $img = fopen($file_name, 'r') or die("cannot read image\n");
